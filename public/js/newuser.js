6b7ccc2f20092935e1aa12c394f19b82008dbcc1
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-03-22 06:41:46 -0400

    semiworking js

--- public/js/newuser.js
+++ public/js/newuser.js
@@ -1,39 +1,37 @@
-  var userType = $("#typeOfuser").val().trim();
-  var firstName = $("#firstName").val().trim();
-  var lastName = $("#lastName").val().trim();
-  var profilePic = $("#profilePic").val().trim();
-  var birthdate = $("#birthdate").val().trim();
-  var emailAddress = $("#email").val().trim();
-  var country = $("#country").val().trim();
-  var religion = $("#religion").val().trim();
-  var languages = $("#languages").val().trim();
-  var lengthOfstay = $("#length").val().trim();
-  var introduction = $("#introduction").val().trim();
+  // var userType = $("#typeOfuser").val().trim();
+  // var firstName = $("#firstName").val().trim();
+  // var lastName = $("#lastName").val().trim();
+  // //var profilePic = $("#profilePic").val().trim();
+  // var birthdate = $("#birthdate").val().trim();
+  // var emailAddress = $("#email").val().trim();
+  // var country = $("#country").val().trim();
+  // var school = $("#school").val().trim();
+  // var religion = $("#religion").val().trim();
+  // var languages = $("#languages").val().trim();
+  // var lengthOfstay = $("#length").val().trim();
+  // var introduction = $("#introduction").val().trim();
 
   $(".btn-primary").on("click", function(event) {
       event.preventDefault();
-  console.log(firstName);
-  console.log(lastName);
-  console.log(emailAddress);
   var newUser = {
-    typeofuser: userType,
-  	firstname: firstName,
-    lastname: lastName,
-    profilepicture: profilePic,
-    birthdate: birthdate,
+    typeofuser: $("#typeOfuser").val().trim(),
+  	firstname: $("#firstName").val().trim(),
+    lastname: $("#lastName").val().trim(),
+    //profilepicture: profilePic,
+    birthdate: $("#birthdate").val().trim(),
     // gender: req.body.
-    emailaddress: emailAddress,
-    country: country,
-    religion: religion,
-    language: languages,
-    length: lengthOfstay,
-    introduction: introduction
+    emailaddress: $("#birthdate").val().trim(),
+    country: $("#country").val().trim(),
+    school: $("#school").val().trim(),
+    religion: $("#religion").val().trim(),
+    language: $("#languages").val().trim(),
+    staylength: $("#length").val().trim(),
+    introduction: $("#introduction").val().trim()
     };
 
   console.log(newUser);
 
-  //submitForm(newUser);
-
+  submitForm(newUser);
 
 
     function deleteInput() {
