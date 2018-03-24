53421e35558edcf0cc6202bc590afbb3caffebf3
jelizabethscott <jescottphoto@gmail.com>
2018-03-24 00:05:09 -0400

    app.put update method added

--- routes/store-api-routes.js
+++ routes/store-api-routes.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
     language: req.body.language,
     staylength: req.body.staylength,
     introduction: req.body.introduction
-  })
+  }) 
   .then(function(dbUser) {
     res.json(dbUser);
   });
@@ -47,6 +47,32 @@ module.exports = function(app) {
       res.json(dbUser);
     });
   });
+
+  app.put("/api/currentUser", function(req.res){
+    db.currentUser.update(
+      req.body,
+      {
+        where: {
+      typeofuser: req.body.typeofuser,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      profilepicture: req.body.profilepicture,
+      birthdate: req.body.birthdate,
+      gender: req.body.gender,
+      emailaddress: req.body.emailaddress,
+      country: req.body.country,
+      school: req.body.school,
+      religion: req.body.religion,
+      language: req.body.language,
+      staylength: req.body.staylength,
+      introduction: req.body.introduction
+        }
+      }).then(function(dbUser) {
+        res.json(dbUser);
+      });
+  });
+};
+
 
   app.get("/api/getcurrent", function(req, res) {
     db.currentUser.findAll({})
